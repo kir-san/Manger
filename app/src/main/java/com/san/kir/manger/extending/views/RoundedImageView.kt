5e9a67317005c3f07b44dee1cf95e388f2da32e0
kir-san <provakator-30@mail.ru>
2018-05-25 14:02:29 +0900

    added rounded image

---
+++ app/src/main/java/com/san/kir/manger/extending/views/RoundedImageView.kt
@@ -0,0 +1,59 @@
+package com.san.kir.manger.extending.views
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
+import android.graphics.Rect
+import android.graphics.drawable.BitmapDrawable
+import android.widget.ImageView
+import kotlin.math.min
+
+class RoundedImageView(context: Context) : ImageView(context) {
+    override fun onDraw(canvas: Canvas) {
+        drawable?.let { drawable ->
+            if (width == 0 || height == 0) return
+            val b = (drawable as BitmapDrawable).bitmap
+            val bitmap = b.copy(Bitmap.Config.ARGB_8888, true)
+
+            val roundBitmap = getCroppedBitmap(bitmap, width)
+            canvas.drawBitmap(roundBitmap, 0F, 0F, null)
+        }
+    }
+
+    private fun getCroppedBitmap(bmp: Bitmap, radius: Int): Bitmap {
+        val sbmp = if (bmp.width != radius || bmp.height != radius) {
+            val smallest: Float = min(bmp.width, bmp.height).toFloat()
+            val factor = smallest / radius
+            Bitmap.createScaledBitmap(
+                bmp,
+                (bmp.width / factor).toInt(),
+                (bmp.height / factor).toInt(),
+                false
+            )
+        } else {
+            bmp
+        }
+
+        val output = Bitmap.createBitmap(radius, radius, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(output)
+
+        val color = "#BAB399"
+        val paint = Paint()
+        val rect = Rect(0, 0, radius, radius)
+
+        paint.isAntiAlias = true
+        paint.isFilterBitmap = true
+        paint.isDither = true
+        canvas.drawARGB(0, 0, 0, 0)
+        paint.color = Color.parseColor(color)
+        canvas.drawCircle(radius / 2 + 0.7f, radius / 2 + 0.7f, radius / 2 + 0.1f, paint)
+        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+        canvas.drawBitmap(sbmp, rect, rect, paint)
+
+        return output
+    }
+}

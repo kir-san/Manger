ef73f49d1a081afdd435a7fc9dde1bb0287102d9
kir-san <provakator-30@mail.ru>
2018-05-31 23:38:07 +0900

    add key for download options, remove manual from sorting

--- app/src/main/java/com/san/kir/manger/utils/variables.kt
+++ app/src/main/java/com/san/kir/manger/utils/variables.kt
@@ -1,40 +1,44 @@
 package com.san.kir.manger.utils
 
+import com.san.kir.manger.App
+import com.san.kir.manger.R
 import java.text.SimpleDateFormat
 import java.util.*
 
-var isFirstRun = true // Временный костыль
-
 // Названия папок
 object DIR {
-    val ROOT = "Manger"
-    val PARSERS = "$ROOT/parsers"
-    val PROFILE = "$ROOT/profile"
-    val CATALOGS = "$ROOT/catalogs"
-    val MANGA = "$ROOT/manga"
-    val LOCAL = "$MANGA/local"
+    const val ROOT = "Manger"
+    const val PROFILE = "$ROOT/profile"
+    const val CATALOGS = "$ROOT/catalogs"
+    const val MANGA = "$ROOT/manga"
+    private const val LOCAL = "$MANGA/local"
     val ALL = listOf(CATALOGS, MANGA, PROFILE, LOCAL)
 }
 
 
 // Настройки
-const val CATEGORY_ALL = "Все"
+val CATEGORY_ALL: String = App.context.getString(R.string.category_all)
 
 // Переключение отображения названия категории в библиотеке
 const val NAME_SHOW_CATEGORY = "show_category"
 
-val sPrefViewer = "ViewerActivity"
-val sPrefListChapters = "ListChaptersActivity"
+const val sPrefViewer = "ViewerActivity"
+const val sPrefListChapters = "ListChaptersActivity"
+
+object PrefDownload {
+    const val mainName = "DownloadManager"
+    const val concurrent = "DownloadConcurrent"
+    const val isRetry = "RetryOnError"
+}
 
 
 // Логи
-val TAG = "myLogs"
+const val TAG = "myLogs"
 
 val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale("en"))
 
 enum class SortLibrary {
     AddTime,
     AbcSort,
-    Populate,
-    Manual
+    Populate
 }

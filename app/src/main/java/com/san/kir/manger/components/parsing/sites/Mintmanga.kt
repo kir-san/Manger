50d75d086ef4673eee610c7ed161eed7db7fb1b2
provakator-30@mail.ru <Elpsykongro199304>
2020-01-04 00:34:10 +0900

    fix

--- app/src/main/java/com/san/kir/manger/components/parsing/sites/Mintmanga.kt
+++ app/src/main/java/com/san/kir/manger/components/parsing/sites/Mintmanga.kt
@@ -4,9 +4,12 @@ import com.san.kir.manger.repositories.SiteRepository
 
 class Mintmanga(siteRepository: SiteRepository) : ReadmangaTemplate(siteRepository) {
     override val name: String = "Mint Manga"
-    override val catalogName: String = "mintmanga.com"
+    override val catalogName: String = "mintmanga.live"
     override var volume = siteRepository.getItem(name)?.volume ?: 0
     override var oldVolume = volume
+
+    override val allCatalogName: List<String>
+        get() = super.allCatalogName + "mintmanga.com"
 
     override suspend fun init() = super.init() as Mintmanga
 }

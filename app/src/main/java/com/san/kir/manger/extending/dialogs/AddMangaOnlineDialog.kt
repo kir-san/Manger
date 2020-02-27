ebef1af9a2af24661fb07a0f5c70e1e6a6eb7230
provakator-30@mail.ru <Elpsykongro199304>
2020-02-27 23:39:54 +0900

    fix text`s validator

--- app/src/main/java/com/san/kir/manger/extending/dialogs/AddMangaOnlineDialog.kt
+++ app/src/main/java/com/san/kir/manger/extending/dialogs/AddMangaOnlineDialog.kt
@@ -2,6 +2,7 @@ package com.san.kir.manger.extending.dialogs
 
 import android.graphics.Color
 import android.view.Gravity
+import android.view.ViewManager
 import androidx.lifecycle.lifecycleScope
 import com.san.kir.ankofork.Binder
 import com.san.kir.ankofork.design.textInputEditText
@@ -30,91 +31,120 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class AddMangaOnlineDialog(ctx: BaseActivity) {
+
+    private val siteNames: List<String> = ManageSites.CATALOG_SITES.map { it.catalogName }
+    private val validate = Binder(siteNames.toString().removeSurrounding("[", "]"))
+    private val check = Binder(false)
+    private val isEnable = Binder(false)
+    var url = ""
+
     init {
-        with(ctx) {
-            alert {
-                val siteNames: List<String> = ManageSites.CATALOG_SITES.map { it.catalogName }
-                val validate = Binder(siteNames.toString().removeSurrounding("[", "]"))
-                val check = Binder(false)
-                val isEnable = Binder(false)
-                var url = ""
+        ctx.showDialog()
+    }
 
-                titleResource = R.string.library_add_manga_title
-                customView {
-                    verticalLayout {
-                        padding = dip(16)
+    private fun BaseActivity.showDialog() {
+        alert {
+            titleResource = R.string.library_add_manga_title
 
-                        horizontalProgressBar {
-                            verticalPadding = dip(5)
-                            isIndeterminate = true
-                            visibleOrGone(check)
-                        }.lparams(height = dip(15), width = matchParent)
+            customView {
+                ui(this@showDialog)
+            }
+        }.show()
+    }
 
-                        textInputLayout {
-                            textInputEditText {
-                                hintResource = R.string.library_add_manga_hint
-                                typeText()
-                                textChangedListener {
-                                    onTextChanged { text, _, _, _ ->
-                                        text?.let {
-                                            validate.item = when {
-                                                text.isNotBlank() -> {
-                                                    url = text.toString()
-                                                    val temp = siteNames
-                                                        .filter { it.contains(text) }
-                                                        .toString()
-                                                        .removeSurrounding("[", "]")
-                                                    if (temp.isNotBlank()) {
-                                                        isEnable.negative()
-                                                        temp
-                                                    } else {
-                                                        isEnable.positive()
-                                                        siteNames
-                                                            .filter { text.contains(it) }
-                                                            .toString()
-                                                            .removeSurrounding("[", "]")
-                                                    }
-                                                }
-                                                else -> {
-                                                    isEnable.negative()
-                                                    siteNames
-                                                        .toString()
-                                                        .removeSurrounding("[", "]")
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
+    private fun ViewManager.ui(ctx: BaseActivity) {
+        verticalLayout {
+            padding = dip(16)
+
+            horizontalProgressBar {
+                verticalPadding = dip(5)
+                isIndeterminate = true
+                visibleOrGone(check)
+            }.lparams(height = dip(15), width = matchParent)
+
+            textInputLayout {
+                textInputEditText {
+                    hintResource = R.string.library_add_manga_hint
+                    typeText()
+                    textChangedListener {
+                        onTextChanged { text, _, _, _ ->
+                            text?.let {
+                                validateUrl(it)
                             }
-                        }
-                        textView(validate) {
-                            textColor = Color.RED
-                        }.lparams {
-                            gravity = Gravity.END
-                        }
-
-                        textView(R.string.library_add_manga_add_btn) {
-                            isEnable.bind { isEnabled = it }
-                            textSize = 17f
-                            padding = dip(10)
-                            onClick {
-                                ctx.lifecycleScope.launch(Dispatchers.Default) {
-                                    check.positive()
-                                    ManageSites.getElementOnline(url)?.also {
-                                        AddMangaDialog(ctx, it)
-                                    } ?: run {
-                                        validate.item = getString(R.string.library_add_manga_error)
-                                    }
-                                    check.negative()
-                                }
-
-                            }
-                        }.lparams {
-                            gravity = Gravity.END
                         }
                     }
                 }
-            }.show()
+            }
+            textView(validate) {
+                textColor = Color.RED
+            }.lparams {
+                gravity = Gravity.END
+            }
+
+            textView(R.string.library_add_manga_add_btn) {
+                isEnable.bind { isEnabled = it }
+                textSize = 17f
+                padding = dip(10)
+                onClick {
+                    ctx.lifecycleScope.launch(Dispatchers.Default) {
+                        check.positive()
+                        ManageSites.getElementOnline(url)?.also {
+                            AddMangaDialog(ctx, it)
+                        } ?: run {
+                            validate.item = ctx.getString(R.string.library_add_manga_error)
+                        }
+                        check.negative()
+                    }
+
+                }
+            }.lparams {
+                gravity = Gravity.END
+            }
         }
     }
+
+    private fun validateUrl(text: CharSequence) {
+        validate.item =
+            if (text.isNotBlank()) {
+                // получаем введенный адрес
+                url = text.toString()
+
+                // список сайтов подходящий под введеный адрес
+                val temp = siteNames
+                    .filter { it.contains(text) }
+                    .toString()
+                    .removeSurrounding("[", "]")
+
+                // Если список не пуст, то отображаем его
+                if (temp.isNotBlank()) {
+                    isEnable.negative()
+                    temp
+                } else {
+                    // Если в списке что-то есть
+                    // то получаем соответствующий сайт
+                    val site = siteNames
+                        .filter { text.contains(it) }
+                        .toString()
+                        .removeSurrounding("[", "]")
+
+                    //Если есть хоть один сайт, то включаем кнопку
+                    if (site.isNotBlank()) {
+                        isEnable.positive()
+                        site
+                    } else {
+                        isEnable.negative()
+                        ""
+                    }
+                }
+            }
+            // Если нет текста, то отображается список
+            // доступных сайтов
+            else {
+                isEnable.negative()
+                siteNames
+                    .toString()
+                    .removeSurrounding("[", "]")
+            }
+    }
 }
+

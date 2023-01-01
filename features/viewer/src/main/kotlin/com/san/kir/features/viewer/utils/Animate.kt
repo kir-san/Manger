76dbaa86cf955743a19356abababd2a25414e592
kir-san <provakatory@gmail.com>
2023-01-01 22:00:04 +1000

    * Добавил скрытие кнопки обновить после ее нажатия * Заменил исплользование анимаций

--- features/viewer/src/main/kotlin/com/san/kir/features/viewer/utils/Animate.kt
+++ features/viewer/src/main/kotlin/com/san/kir/features/viewer/utils/Animate.kt
@@ -1,24 +1,3 @@
 package com.san.kir.features.viewer.utils
 
-import android.animation.Animator
-import android.animation.ValueAnimator
-import android.view.animation.DecelerateInterpolator
-import androidx.core.animation.addListener
-
-internal fun animate(
-    onUpdate: (Float) -> Unit,
-    onStart: (Animator) -> Unit = {},
-    onEnd: (Animator) -> Unit = {},
-) {
-    val animator = ValueAnimator.ofFloat(0f, 200f)
-    animator.duration = 200L
-    animator.interpolator = DecelerateInterpolator()
-
-    animator.addUpdateListener { onUpdate(it.animatedValue as Float) }
-    animator.addListener(
-        onEnd = onEnd,
-        onStart = onStart
-    )
-
-    animator.start()
-}
+const val VIEW_OFFSET = 200f
